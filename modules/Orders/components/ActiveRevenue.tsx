df301ba8a508481e377cb5dbd0cb9ae95a8f22c2
Eric <ericemeka732@gmail.com>
2025-08-14 11:39:40 +0100

    Total revenue replaced with api value - removed dummy data

--- modules/Orders/components/ActiveRevenue.tsx
+++ modules/Orders/components/ActiveRevenue.tsx
@@ -1,11 +1,14 @@
+import { useOrderDataStore } from "../store/useOrderDataStore";
+
 export default function ActiveRevenue() {
+  const {counts, total} = useOrderDataStore()
   return (
     <div className="flex items-center gap-3 text-sm font-medium">
       <p>
-        Active Orders: <span className="text-amber-600">{0}</span>
+        Active Orders: <span className="text-amber-600">{counts.completed + counts.pending}</span>
       </p>
       <p>
-        Revenue: <span className="text-green-500">{0}</span>
+        Revenue: <span className="text-green-500">{total.toLocaleString()}</span>
       </p>
     </div>
   );
